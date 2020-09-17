616f73fbaff706103b4db5f65dfe51fe8fdc65a7
Luélly Cabral <luellycabral21@gmail.com>
2020-09-17 16:04:51 -0300

    Adiciona a letra da musica

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //<---Inicio(Titulo & Cantor)--->//
+             Console.Write("Titulo:  ");
+             Console.ForegroundColor = ConsoleColor.DarkGreen;
+                     Console.Write("Lei da Vida (Cantora - Sabrina Lopes)");
+             Console.Write("\n");
+             Console.ResetColor();
+            //<---Fim(Titulo & Cantor)--->//
+           
+            
+             Console.WriteLine("Tudo que sobe desce");
+             Console.WriteLine("Tudo que entra sai");
+             Console.WriteLine("Hoje você me vê");
+             Console.WriteLine("Amanhã talvez não veja mais");
+             Console.Write("\n");
+
+             Console.WriteLine("Tudo que nasce morre");
+             Console.WriteLine("Tudo que vem se vai");
+             Console.WriteLine("Hoje morreu um sonho");
+             Console.WriteLine("Que ele descanse em paz");
+             Console.Write("\n");
+
+             Console.WriteLine("Não temos controle");
+             Console.WriteLine("O tempo é ligeiro");
+             Console.WriteLine("Na estrada da vida");
+             Console.WriteLine("Somos passageiros");
+             Console.WriteLine("Mas que Deus guarde");
+             Console.WriteLine("Cada estrela mais no céu");
+             Console.Write("\n");
+
+             Console.WriteLine("As lembranças ficam");
+             Console.WriteLine("Não tem como volta");
+             Console.WriteLine("Pra quele momento");
+             Console.WriteLine("Naquele lugar");
+             Console.WriteLine("Aquele Sorriso");
+             Console.WriteLine("Já mais vera de novo");
+             Console.Write("\n");
+
+             //<---Inicio do Refrão 1--->//
+             Console.ForegroundColor = ConsoleColor.DarkGreen;
+             Console.WriteLine("Onde estiver");
+             Console.WriteLine("Olha por mim");
+             Console.WriteLine("O meu amor não vai ter fim");
+             Console.WriteLine("Tudo que passou do meu lado");
+             Console.WriteLine("Estará pra sempre marcado");
+             Console.Write("\n");
+
+
+             Console.WriteLine("Onde estiver");
+             Console.WriteLine("Olha por mim");
+             Console.WriteLine("O meu amor não vai ter fim");
+             Console.WriteLine("Tudo que vem tem que ir");
+             Console.WriteLine("É a lei da vida ");
+             Console.WriteLine("Nãõ é feita por mim");
+             Console.Write("\n");
+             //<---Fim do Refrão 1--->//
+
+             //<---Inicio do Refrão 2--->//
+             Console.WriteLine("Onde estiver");
+             Console.WriteLine("Olha por mim");
+             Console.WriteLine("O meu amor não vai ter fim");
+             Console.WriteLine("Tudo que passou do meu lado");
+             Console.WriteLine("Estará para sempre marcado");
+             Console.Write("\n");
+
+
+             Console.WriteLine("Onde estiver");
+             Console.WriteLine("Olha por mim");
+             Console.WriteLine("O meu amor não vai ter fim");
+             Console.WriteLine("Tudo que vem tem que ir");
+             Console.WriteLine("É a lei da vida ");
+             Console.WriteLine("Não é feita por mim..");
+             Console.Write("\n");
+             Console.ResetColor();
+             //<---Fim do Refrão 2--->//
+
+
+
+        }
+    }
+}
